11c1c03131a40df9e8fb519b84de9928edb4db73
saltysteev <slfarina@att.net>
2021-06-18 15:57:23 -0400

    Error log full implementation, restructure datetime execution, SQLite3 bug fixes

--- ping.py
+++ ping.py
@@ -31,12 +31,30 @@ def create_connection(db):
         conn = sqlite3.connect(db)
         conn.row_factory = sqlite3.Row
         c = conn.cursor()
+        c2 = conn.cursor()
         c.execute('SELECT * FROM servers')
         obj = {'servers': [dict(row) for row in c.fetchall()]}
+        c2.execute('SELECT * FROM events')
+        event_logs = {'events': [dict(row) for row in c2.fetchall()]}
+    except Error as e:
+        print(f'Connection error: {e}')
+    finally:
+        if conn:
+            conn.close()
+
+
+def update_event_dict():
+    global event_logs
+    """ Updates the event_log after every event """
+    conn = None
+    try:
+        conn = sqlite3.connect(DATABASE_PATH)
+        conn.row_factory = sqlite3.Row
+        c = conn.cursor()
         c.execute('SELECT * FROM events')
         event_logs = {'events': [dict(row) for row in c.fetchall()]}
     except Error as e:
-        print(f'Connection error: {e}')
+        print(f'Event update error: {e}')
     finally:
         if conn:
             conn.close()
@@ -58,7 +76,7 @@ def write_db(data, sql):
 
 
 def write_to_file(path, data):
-    """ Write to event log or host file """
+    """ Write to event log or XML file """
     with open(path, 'w') as f:
         f.write(data)
 
@@ -66,12 +84,18 @@ def write_to_file(path, data):
 def change_status(i, dt, online):
     """ Changing status online or offline """
     if i['attempt'] >= 2:  # After three conflicting checks in a row, change status
+        if online:
+            now = datetime.datetime.now()
+            dt_string = now.strftime("%B %d, %Y %-I:%M %p")
+            sql = ''' INSERT INTO events(name,date,duration) VALUES(?,?,?) '''
+            write_db((i['name'], str(dt_string), datetime.timedelta(seconds=(dt - i['time']))), sql)
+            update_event_dict()
         i['status'] = 'online' if online else 'offline'
         i['css'] = 'table-success' if online else 'table-danger'
-        i['time'] = int(dt.timestamp())
+        i['time'] = dt
         i['attempt'] = 0
         sql = ''' UPDATE servers SET time = ?, status = ?, css = ? WHERE name = ? '''
-        write_db((i['name'], int(dt.timestamp()), i['status'], i['css']), sql)
+        write_db((dt, i['status'], i['css'], i['name']), sql)
     else:
         i['attempt'] += 1
 
@@ -79,21 +103,20 @@ def change_status(i, dt, online):
 def monitor_ping(sec):
     while True:
         for i in obj['servers']:
-            dt = datetime.datetime.today()
+            now = datetime.datetime.now()
+            dt = int(datetime.datetime.timestamp(now))
             r = ping(i['ip'], count=1)
             if r.success() and i['status'] == 'online':
                 i['attempt'] = 0
             elif r.success() and i['status'] != 'online':  # An OK check and previous check was NOT OK
-                sql = ''' INSERT INTO events(name,date,duration) VALUES(?,?,?) '''
-                write_db((i['name'], dt.today(), i['duration']), sql)
                 change_status(i, dt, True)
             elif not r.success() and i['status'] == 'offline':
                 i['attempt'] = 0
             elif not r.success() and i['status'] != 'offline':  # A NOT OK check and previous check was OK
                 change_status(i, dt, False)
-            i['duration'] = str(datetime.timedelta(seconds=int(dt.timestamp()) - i['time']))  # Writes datetime readable for dict > XML
+            i['duration'] = str(datetime.timedelta(seconds=(dt - i['time'])))  # Writes datetime readable for dict > XML
             sql = ''' UPDATE servers SET duration = ?, attempt = ? WHERE name = ? '''
-            write_db((i['name'], int(dt.timestamp()) - i['time'], i['attempt']), sql)  # Writes datetime as seconds for database
+            write_db(((dt - i['time']), i['attempt'], i['name']), sql)  # Writes datetime as seconds for database
         xml = dicttoxml(obj, attr_type=False)
         eventxml = dicttoxml(event_logs, attr_type=False)
         write_to_file(EVENTXML_PATH, parseString(eventxml).toprettyxml())
